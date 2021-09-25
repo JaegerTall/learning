9e51e88b34b4841bed94c38184fc088b4d23b5df
Alexander_M <get.sr@inbox.ru>
2021-09-25 13:04:51 +0300

    JS Lesson

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+const planetname = "Земля";
+console.log(planetname);
+
+let NowuserTime = "12:36";
+console.log(NowuserTime);
+
+let NumberCount;
+Count = "10";
+console.log(NumberCount);
+
+let realmoney = "оплата наличными";
+let cardmoney = "оплата по карте";
+console.log(cardmoney);
+console.log(realmoney);
+
+const Fulllname = "Тимофей";
+const familian = "Гавриков";
+const otchestvo = "Олегович";
+console.log(Fullname);
